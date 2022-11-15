c4792c0f9b1f28b58b5dea6cc2ea038fbe370a62
Rioba-Ian-Felix <riobafelix@gmail.com>
2022-11-15 18:42:33 +0300

    "inspiration section, data.js and readme"

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,12 +3,14 @@ import Hero from "../Hero"
 import Logos from "../Logos"
 import Footer from "../Footer"
 import CTA from "../CTA"
+import BNB from "../BNB"
 export default function Home() {
     return (
         <>
             <Navbar />
             <Hero />
             <Logos />
+            <BNB />
             <CTA />
             <Footer />
         </>
